447ea4a896eb7334a06822288cc870c3ef9175ad
Naq <JustSomeEmail.gmail.com>
2019-05-06 15:05:17 +0800

    LOL

---
+++ GroupWork/BranchWorkflow5/BranchWorkflow5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BranchWorkflow5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= a; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
